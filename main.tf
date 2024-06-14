00d14b46a2b2bc5c27bf705934a2c3e82cb9de8a
Gabor Kalmar <gakalmar@gmail.com>
2024-06-14 08:03:07 +0200

    Add cluster to TF file

--- main.tf
+++ main.tf
@@ -57,3 +57,19 @@ resource "aws_security_group" "eks_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# CREATE CLUSTER
+resource "aws_eks_cluster" "eks" {
+  name     = "hello-world-eks-cluster"
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.eks_subnet[*].id
+    security_group_ids = [aws_security_group.eks_sg.id]
+  }
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSVPCResourceController,
+  ]
+}
