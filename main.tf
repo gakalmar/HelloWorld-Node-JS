7dd54955ce3b920cb8ac4daa44d4c1d2065dcecb
Gabor Kalmar <gakalmar@gmail.com>
2024-06-14 08:04:11 +0200

    Add IAM roles and policies to TF file

--- main.tf
+++ main.tf
@@ -73,3 +73,28 @@ resource "aws_eks_cluster" "eks" {
     aws_iam_role_policy_attachment.eks_AmazonEKSVPCResourceController,
   ]
 }
+
+# CREATE IAM ROLES AND POLICIES
+resource "aws_iam_role" "eks" {
+  name = "hello-world-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
