93f2c280a1a89dd9b1009ed42acfeb3619134494
Gabor Kalmar <gakalmar@gmail.com>
2024-06-17 15:45:25 +0200

    Update cluster file with nodegroup

--- terraform/03-eks-cluster.tf
+++ terraform/03-eks-cluster.tf
@@ -16,3 +16,23 @@ resource "aws_eks_cluster" "eks" {
 data "aws_eks_cluster_auth" "eks" {
   name = aws_eks_cluster.eks.name
 }
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "hello-world-eks-node-group"
+  node_role_arn   = aws_iam_role.eks_node.arn
+
+  # Use a string comparison to filter for private subnets
+  subnet_ids      = [for s in aws_subnet.eks_subnet : s.id if can(regex("private", s.tags["Name"]))]
+
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_eks_cluster.eks
+  ]
+}
